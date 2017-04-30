523879e596f5115f3116ff5814a91eaa1bfde7a4
agadar <martin.donkersloot@outlook.com>
2017-04-30 20:18:28 +0200

    Set up unit test skeleton for JavaCommander.
    TODO: Fix JavaCommander docs and implement unit tests.

---
+++ src/test/java/com/github/agadar/javacommander/JavaCommanderTest.java
@@ -0,0 +1,73 @@
+package com.github.agadar.javacommander;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests com.github.agadar.javacommander.JavaCommander.
+ *
+ * @author Agadar (https://github.com/Agadar/)
+ */
+public class JavaCommanderTest {
+
+    public JavaCommanderTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of execute method, of class JavaCommander.
+     */
+    @Test
+    public void testExecute_String() {
+        System.out.println("execute");
+
+    }
+
+    /**
+     * Test of execute method, of class JavaCommander.
+     */
+    @Test
+    public void testExecute_List() {
+        System.out.println("execute");
+
+    }
+
+    /**
+     * Test of registerObject method, of class JavaCommander.
+     */
+    @Test
+    public void testRegisterObject() {
+        System.out.println("registerObject");
+
+    }
+
+    /**
+     * Test of unregisterObject method, of class JavaCommander.
+     */
+    @Test
+    public void testUnregisterObject() {
+        System.out.println("unregisterObject");
+
+    }
+
+}
